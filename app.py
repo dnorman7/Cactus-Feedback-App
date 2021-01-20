2454010ceae097930c684f8818d1981e1dcda264
Deanna Norman <deannaleenorman@msn.com>
2021-01-20 00:30:50 -0800

    adds model

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+  
+if ENV == 'dev':
+  app.debug = True  
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1503548@localhost/cactus' 
+else:
+  app.debug = False
+  app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+  __tablename__ = 'feedback'
+  id = db.Column(db.Integer, primary_key=True)
+  customer = db.Column(db.String(200), unique=True)
+  server = db.Column(db.String(200))
+  rating = db.Column(db.Integer)
+  comments = db.Column(db.text())
+
+  def __init__(self, customer, server, rating, comments):
+    self.customer = customer
+    self.server = server
+    self.rating = rating
+    self.comments = comments
 
 @app.route('/')
 def index():
@@ -19,5 +47,5 @@ def submit():
     return render_template('success.html')
 
 if __name__ == '__main__':
-    app.debug = True
+    
     app.run()
